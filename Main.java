8b5fc87aebcd9708e1680065f33caf4755be32fe
lugr12 <luisegross97@googlemail.com>
2025-05-21 12:32:01 +0200

    Multiplikation umgesetzt

--- Main.java
+++ Main.java
@@ -4,6 +4,14 @@ public class Main
 {
     public static void main (String args[])
     {
+        int obereGrenze = 100;
+        int mult = 1;
         
+        for (int i = 1; i <= obereGrenze; i++)
+        {
+            mult *= 1;
+        }
+        
+        System.out.println("Mult = " + mult);
     }
 }
